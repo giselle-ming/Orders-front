5d9a948a283b5c320da1bd3b633efe2547c32c0a
giselle <giselle.mingue@gmail.com>
2024-04-21 17:09:14 -0400

    fix

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'primereact/button';
 import { PDFDownloadLink, Document, Page, Text, View } from '@react-pdf/renderer';
 import { useToken } from '../context/TokenContext';
